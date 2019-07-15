7c97af7fbd15881d16c1748be8d5b23a0773362f
Thomas Cheng <thomas.cheng@loopio.com>
2019-07-14 22:26:13 -0400

    Prevent default drag actions

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
+
+window.addEventListener(
+  "touchmove",
+  evt => {
+    evt.preventDefault();
+  },
+  { passive: false }
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
